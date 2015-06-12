247672b55e0f02114fa1e104b8763c030ac15251
alexey-agafonov <aleks.xploid@gmail.com>
2015-06-12 19:27:20 +0300

    Add a 12th exercise

---
+++ Ex12.scala
@@ -0,0 +1,19 @@
+import scala.math._
+
+object HelloWorld {
+  def main(args: Array[String]) = {
+    def abc(list: List[Double]): Double = {
+
+      def acc(list: List[Double], mult: Double, sum: Double): Double = list match {
+        case Nil => mult * sum
+        case x :: tail => {
+          acc(tail, mult * x, sum + sin(x))
+        }
+      }
+      acc(list, 1, 0)
+    }
+
+    val a = List(1.0, 2, 3, 4)
+    print(abc(a))
+  }
+}
